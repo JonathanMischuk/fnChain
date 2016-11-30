aebcc8f57ef1cc82baeb16549c6380d3602f2db4
JonathanMischuk <jonathan.mischuk@gmail.com>
2016-11-30 18:18:06 -0500

    smal changes

--- config/app.server.js
+++ config/app.server.js
@@ -1,12 +1,9 @@
 'use strict';
 
-var express = require('express'),
-    bodyParser = require('body-parser');
+var express = require('express');
 
 module.exports = function () {
     var app = express();
-
-    //app.use(bodyParser.json());
 
     // api routing
     require('./routes.server')(app);
